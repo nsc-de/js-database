d042afdb2f3a24851dfb11cec1eea732b2fa4fbf
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2020-11-06 09:32:29 +0100

    Created quickstart examples for using the xml-adapter

---
+++ example/javascript/xml-quickstart.js
@@ -0,0 +1,45 @@
+let { createDatabase } = require('nscdb');
+let { XMLFileAdapter } = require('nscdb/xml_adapter');
+
+async function myFunction() {
+
+  let database = await createDatabase(new XMLFileAdapter("./database.xml"));
+
+  // Set defaults
+  database.setDefaults({
+    users: []
+  });
+
+  // Push a value into the Database
+  let users = database.get("users");
+  users.push({
+    id: database.generateId("users"),
+    name: 'Harleen Dolan',
+    password: 'a password'
+  });
+  
+  users.push({
+    id: database.generateId("users"),
+    name: 'Lisa Bradley',
+    password: 'another password'
+  });
+
+  // print output data from the database
+  console.log(database.data);
+
+  // Console Output:
+  // >> {
+  // >>   users: [
+  // >>     { id: 0, name: 'Harleen Dolan', password: 'a password' },
+  // >>     { id: 1, name: 'Lisa Bradley', password: 'another password' }
+  // >>   ],
+  // >>   id_counters: { users: 1 }
+  // >> }
+
+
+  // Save the Database
+  await database.saveData();
+
+}
+
+myFunction();
